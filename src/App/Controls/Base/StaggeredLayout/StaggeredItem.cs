1c80c6d836d35c7f1a1281b16a8e591469006acd
Anran Zhang <Thansy@foxmail.com>
2023-10-10 11:20:29 +0800

    处理动态条目

---
+++ src/App/Controls/Base/StaggeredLayout/StaggeredItem.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Bili Copilot. All rights reserved.
+
+using Microsoft.UI.Xaml;
+
+namespace Bili.Copilot.App.Controls.Base;
+
+internal class StaggeredItem
+{
+    public StaggeredItem(int index) => Index = index;
+
+    public double Top { get; internal set; }
+
+    public double Height { get; internal set; }
+
+    public int Index { get; }
+
+    public UIElement Element { get; internal set; }
+}
