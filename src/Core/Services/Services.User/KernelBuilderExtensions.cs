5b7d4e408c35abb151aa0cb0be3348fd1ff3c43c
Richasy <Thansy@foxmail.com>
2024-07-04 21:20:27 +0800

    Update

---
+++ src/Core/Services/Services.User/KernelBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Richasy. All rights reserved.
+
+using Microsoft.Extensions.DependencyInjection;
+using Richasy.BiliKernel.Bili.User;
+using Richasy.BiliKernel.Services.User;
+
+namespace Richasy.BiliKernel;
+
+/// <summary>
+/// 内核构建器扩展.
+/// </summary>
+public static class KernelBuilderExtensions
+{
+    /// <summary>
+    /// 添加用户资料服务.
+    /// </summary>
+    public static IKernelBuilder AddUserProfileService(this IKernelBuilder builder)
+    {
+        builder.Services.AddSingleton<IUserProfileService, UserProfileService>();
+        return builder;
+    }
+}
---
+++ src/Core/Services/Services.User/UserProfileService.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Richasy. All rights reserved.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Richasy.BiliKernel.Authenticator;
+using Richasy.BiliKernel.Bili;
+using Richasy.BiliKernel.Bili.Authorization;
+
+
+// Copyright (c) Richasy. All rights reserved.
+
+using Richasy.BiliKernel.Bili.User;
+using Richasy.BiliKernel.Http;
+using Richasy.BiliKernel.Models.User;
+using Richasy.BiliKernel.Services.User.Core;
+
+namespace Richasy.BiliKernel.Services.User;
+
+/// <summary>
+/// 用户资料服务.
+/// </summary>
+public sealed class UserProfileService : IUserProfileService
+{
+    private readonly MyClient _myClient;
+
+    /// <summary>
+    /// 初始化 <see cref="UserProfileService"/> 类的新实例.
+    /// </summary>
+    public UserProfileService(
+        BiliHttpClient biliHttpClient,
+        IAuthenticationService authenticationService,
+        BasicAuthenticator basicAuthenticator)
+    {
+        _myClient = new MyClient(biliHttpClient, authenticationService, basicAuthenticator);
+    }
+
+    /// <inheritdoc/>
+    public Task<UserDetailProfile> GetMyProfileAsync(UserExecutionSettings? executionSettings = null, CancellationToken cancellationToken = default)
+        => _myClient.GetMyInformationAsync(cancellationToken);
+}
