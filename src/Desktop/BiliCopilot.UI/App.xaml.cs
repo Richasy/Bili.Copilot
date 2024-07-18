fd2b9950f69a2ac7875af3d6422c09d9592b9a29
Richasy <Thansy@foxmail.com>
2024-07-18 23:36:54 +0800

    Update

---
+++ src/Desktop/BiliCopilot.UI/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Bili Copilot. All rights reserved.
+
+namespace BiliCopilot.UI;
+
+/// <summary>
+/// Provides application-specific behavior to supplement the default Application class.
+/// </summary>
+public partial class App : Application
+{
+    private Window _window;
+
+    /// <summary>
+    /// Initializes the singleton application object.  This is the first line of authored code
+    /// executed, and as such is the logical equivalent of main() or WinMain().
+    /// </summary>
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    /// <summary>
+    /// Invoked when the application is launched.
+    /// </summary>
+    /// <param name="args">Details about the launch request and process.</param>
+    protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+    {
+        _window = new MainWindow();
+        _window.Activate();
+    }
+}
---
+++ src/Desktop/BiliCopilot.UI/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Bili Copilot. All rights reserved.
+
+namespace BiliCopilot.UI;
+
+/// <summary>
+/// An empty window that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class MainWindow : Window
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MainWindow"/> class.
+    /// </summary>
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
