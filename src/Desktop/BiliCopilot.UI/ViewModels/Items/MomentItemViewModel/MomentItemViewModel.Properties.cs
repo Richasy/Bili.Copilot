0f4db1c7ce2f89ac75026dfa70481e3fdf0ef390
Richasy <Thansy@foxmail.com>
2024-08-07 09:59:04 +0800

    Add

---
+++ src/Desktop/BiliCopilot.UI/ViewModels/Items/MomentItemViewModel/MomentItemViewModel.Properties.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Bili Copilot. All rights reserved.
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace BiliCopilot.UI.ViewModels.Items;
+
+/// <summary>
+/// 动态条目视图模型.
+/// </summary>
+public sealed partial class MomentItemViewModel
+{
+    [ObservableProperty]
+    private double? _likeCount;
+
+    [ObservableProperty]
+    private double? _commentCount;
+
+    [ObservableProperty]
+    private bool _isLiked;
+}
---
+++ src/Desktop/BiliCopilot.UI/ViewModels/Items/MomentItemViewModel/MomentItemViewModel.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Bili Copilot. All rights reserved.
+
+using Richasy.BiliKernel.Models.Moment;
+using Richasy.WinUI.Share.ViewModels;
+
+namespace BiliCopilot.UI.ViewModels.Items;
+
+/// <summary>
+/// 动态条目视图模型.
+/// </summary>
+public sealed partial class MomentItemViewModel : ViewModelBase<MomentInformation>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MomentItemViewModel"/> class.
+    /// </summary>
+    public MomentItemViewModel(MomentInformation data)
+        : base(data)
+    {
+        IsLiked = data.CommunityInformation?.IsLiked ?? false;
+        LikeCount = data.CommunityInformation?.LikeCount;
+        CommentCount = data.CommunityInformation?.CommentCount;
+    }
+}
