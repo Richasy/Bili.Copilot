0cd24eefb8e41d43e33eeb7bb3252913162e34e5
Richasy <Thansy@foxmail.com>
2024-08-07 17:16:09 +0800

    Update

---
+++ src/Desktop/BiliCopilot.UI/Extensions/MomentSectionTemplateSelector.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Bili Copilot. All rights reserved.
+
+using BiliCopilot.UI.ViewModels.Items;
+
+namespace BiliCopilot.UI.Extensions;
+
+internal sealed class MomentSectionTemplateSelector : DataTemplateSelector
+{
+    public DataTemplate VideoTemplate { get; set; }
+
+    public DataTemplate ComprehensiveTemplate { get; set; }
+
+    protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+    {
+        if (item is not IMomentSectionDetailViewModel)
+        {
+            return default;
+        }
+
+        var vm = (IMomentSectionDetailViewModel)item;
+        return vm.SectionType == Models.Constants.MomentSectionType.Video ? VideoTemplate : ComprehensiveTemplate;
+    }
+}
