bde93cd3363bdbadf45c5c41a4d4eeebfa0da037
Anran Zhang <anranzhang@microsoft.com>
2024-08-19 14:45:40 +0800

    u

---
+++ src/Desktop/BiliCopilot.UI/Controls/Settings/PlayerBehaviorSettingControl.xaml.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Bili Copilot. All rights reserved.
+
+namespace BiliCopilot.UI.Controls.Settings;
+
+/// <summary>
+/// 播放器行为设置控件.
+/// </summary>
+public sealed partial class PlayerBehaviorSettingControl : SettingsPageControlBase
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PlayerBehaviorSettingControl"/> class.
+    /// </summary>
+    public PlayerBehaviorSettingControl() => InitializeComponent();
+}
