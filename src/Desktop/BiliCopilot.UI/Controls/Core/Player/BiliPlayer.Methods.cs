73ee21cfd268d8e8f452dfdc14c586faeefe6bfa
Richasy <Thansy@foxmail.com>
2024-09-01 10:48:54 +0800

    修复字幕初始大小 (#476)

--- src/Desktop/BiliCopilot.UI/Controls/Core/Player/BiliPlayer.Methods.cs
+++ src/Desktop/BiliCopilot.UI/Controls/Core/Player/BiliPlayer.Methods.cs
@@ -49,14 +49,14 @@ public sealed partial class BiliPlayer
 
         const int initialWidth = 1280;
         const int initialFontSize = 28;
-        const int initialPaddingLeft = 20;
-        const int initialPaddingTop = 8;
+        const int initialPaddingLeft = 16;
+        const int initialPaddingTop = 12;
 
         // 根据实际宽度调整字幕大小，线性增长，每增加100px，字体大小增加3，横向边距增加8，纵向边距增加6.
         var width = ActualWidth;
         SubtitleControls.FontSize = Math.Max(12, (double)(initialFontSize + ((width - initialWidth) / 100 * 1.5)));
         var horizontalPadding = Math.Max(8, initialPaddingLeft + ((width - initialWidth) / 100 * 2.5));
-        var verticalPadding = Math.Max(6, initialPaddingTop + ((width - initialWidth) / 100 * 1));
+        var verticalPadding = Math.Max(8, initialPaddingTop + ((width - initialWidth) / 100 * 1));
         SubtitleControls.Padding = new Thickness(horizontalPadding, verticalPadding, horizontalPadding, verticalPadding);
     }
 
