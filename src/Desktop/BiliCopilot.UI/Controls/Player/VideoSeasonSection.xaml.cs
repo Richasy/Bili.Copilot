faccd314d22985e07e4ab8fce898faf1872ad1a5
Richasy <Thansy@foxmail.com>
2024-10-18 09:16:16 +0800

    修复合集视频切换时无法重新选择初始视频的问题 (#834)

--- src/Desktop/BiliCopilot.UI/Controls/Player/VideoSeasonSection.xaml.cs
+++ src/Desktop/BiliCopilot.UI/Controls/Player/VideoSeasonSection.xaml.cs
@@ -38,6 +38,7 @@ public sealed partial class VideoSeasonSection : VideoSeasonSectionBase
         var item = sender.SelectedItem as VideoItemViewModel;
         if (item is not null && ViewModel.SelectedItem != item)
         {
+            ViewModel.SelectedItem = item;
             ViewModel.Page.InitializePageCommand.Execute(new VideoSnapshot(item.Data));
         }
     }
